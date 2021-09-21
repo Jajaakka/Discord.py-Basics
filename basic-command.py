827d1ebb8b093440954834e5433f0e1cfed24142
Jajaakka <74411065+Jajaakka@users.noreply.github.com>
2021-09-21 11:44:27 +0700

    Create basic-command.py

---
+++ basic-command.py
@@ -0,0 +1,12 @@
+import discord
+import os
+from keep_alive import keep_alive
+
+bot = commands.Bot(command_prefix="?")
+
+@bot.command()
+async def hi(ctx):
+  await ctx.send('hello.')
+
+keep_alive()
+bot.run(os.getenv('TOKEN'))
